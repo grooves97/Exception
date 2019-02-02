d716689ca185860ae7b0233a66475006bac916da
grooves97 <45422778+grooves97@users.noreply.github.com>
2019-02-02 20:23:27 +0600

    HM timless

---
+++ ExceptionHomeWork/ExceptionHomeWork/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Insert the divisor");
+                string dividendString = Console.ReadLine();
+                Console.WriteLine("Insert the dividend");
+                string dividerString = Console.ReadLine();
+
+                if (dividerString==""&&dividendString=="")
+                {
+                    throw new ArgumentException { };
+                }
+
+                int dividentInt = int.Parse(dividendString);
+                int dividerInt = int.Parse(dividerString);
+
+                if (dividerInt == 0)
+                {
+                    throw new ArgumentNullException { };
+                }
+
+                int result = dividentInt / dividerInt;
+                Console.WriteLine($"Result = {result}");
+
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Divider = 0");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Emty");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Новая папка/Array/Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int arraySize = 10;
+                int largeArraySize = 13;
+                int[] someArray = new int[arraySize];
+
+                for(int i = 0; i < largeArraySize; i++)
+                {
+                    if (i > arraySize)
+                    {
+                        throw new IndexOutOfRangeException { };
+                    }
+                    Console.WriteLine(someArray[i]);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
